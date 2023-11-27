35affec79c42faddcc77fc111eac97cf9d351859
Амарбилэг Амартайван <HP230431@G303-CC0128.local>
2023-11-27 18:06:37 +0800

    .

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const users = [
+  { name: "Bataa", id: "12" },
+  { name: "Dulmaa", id: "14" },
+  { name: "Boldoo", id: "23" },
+  { name: "Amraa", id: "16" },
+];
 
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
+app.get("/users", (req, res) => {
+  const { id } = req.params;
+  res.json(users);
+});
+
+app.get("/user/:name", (req, res) => {
+  const { name } = req.params;
+  const user = users.filter((user) => user.name == name);
+
+  res.json(user);
+});
+app.post("/users", (req, res) => {
+  const { name, id } = req.body;
+  users.push({ name, id })
+  res.json(users);
+});
+
+app.listen(3000);
